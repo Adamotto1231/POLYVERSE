c5cf7596f48f6f58f1e311c1e610fa90d4874d19
Adamotto Adamotto <samsib1388@gmail.com>
2025-12-02 16:59:34 +0330

    Initial commit

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Arsam!");
+Console.WriteLine("Hello, Oni-chan!");
+string greeting = "good day!";
+Console.WriteLine(greeting);
+string friendonename = "    Beni   ";
+string friendtwoname = "Asher";
+friendonename = friendonename.Trim();
+// Console.WriteLine("My friends are" + {friendOneName} +  {friendTwoName} + ".");
+string friendsMessage = $"My friends are {friendonename} and {friendtwoname}.";
+Console.WriteLine(friendsMessage);
+// if u reading this, please don't open program.cs it's dangerous.
