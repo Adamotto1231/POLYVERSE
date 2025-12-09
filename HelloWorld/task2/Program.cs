dac213c9eab5fa6e0ca7c6244bcae41200e78475
Adamotto Adamotto <samsib1388@gmail.com>
2025-12-09 17:00:11 +0330

    lmao

--- HelloWorld/task2/Program.cs
+++ HelloWorld/task2/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string command = " system is hacked ";
+command = command.Trim();
+command = command.Replace("system is hacked","system is secure");
+Console.WriteLine(command);
+bool issystemsecure = command.Contains("system is secure");
+Console.WriteLine(issystemsecure);
+int dead = 2140000000;
+int alive = 214000000;
+double a = 12.5;
+float b = 5.02f;
+Console.WriteLine(a / b);
+int totaldead = checked(dead + alive);
+Console.WriteLine(totaldead);
