97fefbe6baba3cc7ca2edfd5dd317a8c5e448a82
Adamotto Adamotto <samsib1388@gmail.com>
2026-02-16 12:23:25 +0330

    initial commit

---
+++ HelloWorld/task3/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Threading.Tasks.Dataflow;
+namespace Typrwriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "hello there. I'm Raga the golden dragon";
+
+            Typewriter(message, 100);
+
+            Console.WriteLine("\nI'm leader of the raga luara guild.");
+            Console.WriteLine("\nhello. how are you");
+            Console.WriteLine("Hatsune Miku is Best");
+            Console.ReadKey();
+    
+    
+        }
+        
+        static void Typewriter(string text, int delay)
+        {
+            foreach (char c in text)
+            {
+                Console.Write(c);
+                Thread.Sleep(delay);
+            }
+        }
+    }
+}
---
+++ HelloWorld/task4/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Teto weather report");
+int temp = 60;
+string wheatherStat = "rainy";
+float friction = 1f;
+if (temp==10)
+{
+    friction= 1.8f;
+    Console.WriteLine("today is a sunny day and the road friction is: " + friction);
+}
+
+else if (temp<50)
+{
+    friction= 0.1f;
+    Console.WriteLine("today is rainy day and the road friction is: " + friction);
+}
+else
+{
+    Console.WriteLine("weather is cold");
+}
+
+// I hate you VScode <3
